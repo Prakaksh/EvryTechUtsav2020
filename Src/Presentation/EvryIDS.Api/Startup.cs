60ab45762550e3e4e219d2c73feabb44494e9864
Shashidhar Jadhav <Shashidhar.Jadhav@evryindia.in>
2020-12-14 13:21:33 +0530

    JWT Token implementation

--- Src/Presentation/EvryIDS.Api/Startup.cs
+++ Src/Presentation/EvryIDS.Api/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EvryIDS.Api.Controllers;
 using EvryIDS.Application.Interfaces;
 using EvryIDS.Application.Repositories;
 using Microsoft.AspNetCore.Builder;
@@ -13,6 +14,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Logging;
 
 namespace EvryIDS.Api
 {
@@ -28,13 +31,50 @@ namespace EvryIDS.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            
             services.AddSingleton<IUser, UserRepository>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "EvryIDS.Api", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                               Type = ReferenceType.SecurityScheme,
+                               Id = "Bearer"
+                            }
+                        },
+                            new string[] { }
+                    }
+                }); 
+            });
+
+            IdentityModelEventSource.ShowPII = true;
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "JwtBearer";
+                options.DefaultChallengeScheme = "JwtBearer";
+            }).AddJwtBearer("JwtBearer", jwtOptions =>
+            {
+                jwtOptions.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    IssuerSigningKeys = (IEnumerable<SecurityKey>)TokenController.SIGNING_KEY,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.FromMinutes(5)
+                };
             });
         }
 
@@ -58,6 +98,8 @@ namespace EvryIDS.Api
             {
                 endpoints.MapControllers();
             });
+
+            app.UseAuthentication();
         }
     }
 }
